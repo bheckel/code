1aa6475799b3764b6f0161ff2ac24f042fa95f7c
Bob Heckel <rsh@rshdev.com>
2018-02-24 07:46:09 -0500

    mod

--- misccode/full_filesystem.sh
+++ misccode/full_filesystem.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-# Warn the users that the file system is getting full.
-#
-# Monitor all the file systems mounted and report to RECEIVER
-#
-# Usage: as a cron entry for best use.
-
-RECEIVER=nachiappan.ramanathan@aig.com
 
 for fs in `df -k | awk '{print $1}' | sed -n "3,14 p"`; do
   x=`df -kl | grep $fs | awk '{ print $5 }'`
@@ -13,6 +6,6 @@ for fs in `df -k | awk '{print $1}' | sed -n "3,14 p"`; do
 
   if [ $x -gt $y ]; then
     message="File System `df -k |grep $fs |awk '{print $6\", \"$5}'` Full!!!"
-    echo $message | mailx -s "`hostname` - File System Full Warning !!!"  $RECEIVER
+    echo $message | mailx -s "`hostname` - File System Full Warning !!!"  wtf@pig.com
   fi
 done
