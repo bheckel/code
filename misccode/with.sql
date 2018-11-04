6b72a8b86c45ca7c9b169fcfba75beae636ca63a
Bob Heckel <rsh@rshdev.com>
2018-11-04 08:20:41 -0500

    mod

--- misccode/with.sql
+++ misccode/with.sql
@@ -1,3 +1,17 @@
+with q as (
+          select date '2000-01-01' d, 10 amt from dual
+union all select date '2000-01-02', 11 from dual
+union all select date '2000-01-03', 30 from dual
+union all select date '2000-01-04', 10 from dual
+union all select date '2000-01-05', 14 from dual
+)
+select d,
+      ,amt
+      ,avg(amt) over (order by d rows between 1 preceding and 1 following) ex1
+      ,sum(amt) over (order by d rows between unbounded preceding and current row) ex2
+from q;
+
+---
 
 with a as(
   select sys_connect_by_path (job, '/') job_path,sys_connect_by_path (ename, '/') ename_path
