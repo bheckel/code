2b04f910221e7cc7a3537f85ff9c79f499282950
Bob Heckel <rsh@rshdev.com>
2019-06-25 12:05:29 -0400

    mod gitall

--- misccode/listagg.sql
+++ misccode/listagg.sql
@@ -6,7 +6,7 @@
 SELECT DISTINCT account_name_id,
                 listagg(eb.event_id,',') WITHIN GROUP(ORDER BY eb.event_id) OVER(PARTITION BY eb.account_name_id) event_id_list,
                 listagg(eb.eventname,',') WITHIN GROUP(ORDER BY eb.event_id) OVER(PARTITION BY eb.account_name_id) eventname_list
-           -- Rank by account & event to use as a limit.  Won't work in the WHERE clause.
+           -- Rank by account & event to use as a limit to avoid >4000 chars.  Won't work in the WHERE clause.
            FROM ( SELECT account_name_id, event_id, eventname, dense_rank() OVER (PARTITION BY account_name_id order by event_id) rownbr FROM event_base ) eb 
           WHERE eb.account_name_id IS NOT NULL 
             AND rownbr < 50  -- avoid 4000 byte char max on the CSV list
