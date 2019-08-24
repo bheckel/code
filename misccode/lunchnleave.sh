724e2a2ad617ba4da21fb0395fc5731629ce86c5
Bob Heckel <rsh@rshdev.com>
2019-08-24 11:09:52 -0400

    mod gitall

--- misccode/lunchnleave.sh
+++ misccode/lunchnleave.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if [ `date +%u` -ne 7 -a `date +%u` -ne 6 ];then  # weekday
-  if [ `date +%H` -eq 12 -a `date +%m` -lt 30 -o `date +%H` -eq 16 -a `date +%m` -gt 15 ];then
-    echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+if $HOSTNAME -ne 'appa';then
+  if [ `date +%u` -ne 7 -a `date +%u` -ne 6 ];then  # weekday
+    if [ `date +%H` -eq 12 -a `date +%m` -lt 30 -o `date +%H` -eq 16 -a `date +%m` -gt 15 ];then
+      echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+    fi
   fi
 fi
