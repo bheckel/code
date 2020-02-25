e4516e3a8db560b827b279275dcbdc70d2cebab1
Bob Heckel <rsh@rshdev.com>
2020-02-25 08:51:41 -0500

    mod gitall x86_64-unknown-cygwin

--- misccode/jira.sh
+++ misccode/jira.sh
@@ -11,14 +11,14 @@ TAG=${JIRA}_${DESC}
 mkdir -p ~/onedrive/orion-${TAG} && cd ~/onedrive/orion-${TAG} && \
 cp -i ~/onedrive/template_t.sql ~/onedrive/orion-${TAG}/${JIRA}.sql && \
 
-# cp -i ~/onedrive/template_bulkcollect.pck ~/onedrive/orion-${TAG}/ORION${JIRA}.pck && \
-cp -i ~/onedrive/template_bulkcollect.pck ~/onedrive/orion-${TAG}/ORION${JIRA}.plsql && \
+# cp -i ~/onedrive/template_t.pck ~/onedrive/orion-${TAG}/ORION${JIRA}.pck && \
+cp -i ~/onedrive/template_t.pck ~/onedrive/orion-${TAG}/ORION${JIRA}.pck && \
 # sed -i "s/99999/${JIRA}/" ORION${JIRA}.pck && \
-sed -i "s/99999/${JIRA}/" ORION${JIRA}.plsql && \
+sed -i "s/99999/${JIRA}/" ORION${JIRA}.pck && \
 
-echo '--SQL> cd C:\Orion\workspace\data\Source\SQL\xOrionScripts\' >> ${JIRA}.sql
-echo '--SQL> @ORION-'${JIRA}'_ddl_change.sql' >> ${JIRA}.sql
-echo "--SQL> DROP PACKAGE ORION${JIRA};" >> ${JIRA}.sql
+echo '--  cd C:\Orion\workspace\data\Source\SQL\xOrionScripts\' >> ${JIRA}.sql
+echo '--  @ORION-'${JIRA}'_ddl_change.sql' >> ${JIRA}.sql
+echo "--  DROP PACKAGE ORION${JIRA};" >> ${JIRA}.sql
 echo >> ${JIRA}.sql
 
 echo "-- ${DESC} https://esapps.sas.com/jira/browse/ORION-${JIRA}" >> ${JIRA}.sql
