d22bffc48fc411fbb705297c483ef800b7963be7
Bob Heckel <rsh@rshdev.com>
2020-07-09 17:14:23 -0400

    mod gitall x86_64-unknown-cygwin

--- misc/jira.sh
+++ misc/jira.sh
@@ -25,7 +25,7 @@ echo >> ${JIRA}.sql
 echo "-- ${DESC} https://esapps.sas.com/jira/browse/ORION-${JIRA}" >> ${JIRA}.sql
 echo '' >> ${JIRA}.sql
 echo "-- $ git checkout develop && git pull && git checkout -b feature/ORION-${JIRA} && git push --set-upstream origin feature/ORION-${JIRA} && git checkout feature/ORION-${JIRA}" >> ${JIRA}.sql
-echo "-- $ git add . && git commit -m ' ORION-"${JIRA}: "' && git push" >> ${JIRA}.sql
+echo "-- $ git add . && git commit -m 'ORION-"${JIRA}: "' && git push" >> ${JIRA}.sql
 echo "-- WITH GITHUB UI: git branch -d feature/ORION-${JIRA} && git fetch -p && git branch -a" >> ${JIRA}.sql
 echo "-- WITHOUT GITHUB UI: git checkout develop && git pull && git merge --no-ff feature/ORION-${JIRA} && git push && git branch -d feature/ORION-${JIRA} && git fetch -p && git push origin --delete feature/ORION-${JIRA}" >> ${JIRA}.sql
 echo "-- Pushed and ran ORION-${JIRA}_ddl_change.sql on ESD" >> ${JIRA}.sql
