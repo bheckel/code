b6edaef61e232fa26d73598f0fd347ac38d38191
Bob Heckel <rsh@rshdev.com>
2022-06-17 16:08:52 -0400

    mod gitall x86_64-unknown-cygwin-l10g

---
+++ scripting/snip.sh
@@ -0,0 +1,16 @@
+snip() {
+  D=$HOME/onedrive/misc/bkup/
+  if [ $# -eq 0 ];then
+    #cp -v 'C:\Documents and Settings\bheck\Application Data\SQL Developer\UserSnippets.xml' $D
+    cp -v 'C:\Users\bheck\AppData\Roaming\SQL Developer\UserSnippets.xml' $D
+    #cp -v 'C:\Documents and Settings\bheck\Application Data\SQL Developer\CodeTemplate.xml' $D
+    cp -v 'C:\Users\bheck\AppData\Roaming\SQL Developer\CodeTemplate.xml' $D
+    #cp -v 'C:\Documents and Settings\bheck\Application Data\SQL Developer\CodeTemplate.xml' $HOME/code/database/
+    cp -v 'C:\Users\bheck\AppData\Roaming\SQL Developer\CodeTemplate.xml' $HOME/code/database/
+    #cp -v "C:\Users\bheck\AppData\Roaming\SQL Developer\system21.4.1.349.1822\o.sqldeveloper\preferences.xml" $D
+    #cp -v "C:\Users\bheck\AppData\Roaming\SQL Developer\system21.4.1.349.1822\o.sqldeveloper\product-preferences.xml" $D
+  else
+    #vim -R 'C:\Documents and Settings\bheck\Application Data\SQL Developer\UserSnippets.xml'
+    vim -R '"C:\Users\bheck\AppData\Roaming\SQL Developer\UserSnippets.xml"'
+  fi
+}
