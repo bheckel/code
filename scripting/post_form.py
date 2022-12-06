832cc5b0ec99468533b06af7901347484dc2c338
Bob Heckel <rsh@rshdev.com>
2022-12-05 20:10:52 -0500

    mod gitall x86_64-unknown-cygwin-mett

--- scripting/post_form.py
+++ scripting/post_form.py
@@ -1,12 +1,13 @@
+# Adapted: 05-Dec-2022 (Bob Heckel - https://www.youtube.com/watch?v=StmNWzHbQJU&list=WL&index=15&t=340s)
 import requests
 
 #url = 'http://rshdev.com/cgi-bin/printenv.pl'
-url = 'http://bheckel.sdf.org/np.pl'
+url = 'http://bheckel.f.org/np.pl'
 
 data = {
   'myposttextbox': 'lkj'
 }
 
-response = requests.post(url, data=data).text
-
-print(response)
+while True:
+    response = requests.post(url, data=data).text
+    print(response)
