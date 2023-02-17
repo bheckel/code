e28c839c303bb83a222d418df19c0245f153e9ab
Bob Heckel <rsh@rshdev.com>
2023-02-17 16:47:05 -0500

    mod gitall x86_64-unknown-cygwin-l10g

--- database/materialized_view.sql
+++ database/materialized_view.sql
@@ -1,6 +1,49 @@
--- https://asktom.oracle.com/pls/asktom/f?p=100:11:0::::P11_QUESTION_ID:9538465900346087401
--- Modified: 02-Oct-2019 (Bob Heckel)
+-- Created: 02-Oct-2019 (Bob Heckel)
+-- Modified: 13-Feb-2023 (Bob Heckel)
 
+---
+
+SELECT owner, mview_name, last_refresh_date
+  FROM all_mviews
+ WHERE owner ='SETARS'
+   AND mview_name in('MYMV')
+  ORDER BY 2;
+
+select owner, name, last_refresh, error, status, refresh_mode  
+  from all_snapshots 
+ where owner ='SETARS';
+---
+
+--https://oracle-base.com/articles/misc/materialized-views
+SELECT * FROM SCOTT.emp;
+
+CREATE MATERIALIZED VIEW scotts_emp_mv
+  BUILD IMMEDIATE 
+  REFRESH FORCE
+  ON DEMAND
+  AS
+    SELECT * FROM SCOTT.emp;
+    
+BEGIN
+  DBMS_STATS.gather_table_stats(
+    ownname => 'ADMIN',
+    tabname => 'SCOTTS_EMP_MV');
+END;
+
+SELECT * FROM scotts_emp_mv;
+
+SELECT */*insert*/ FROM SCOTT.emp;
+
+Insert into SCOTT.EMP (EMPNO,ENAME,JOB,MGR,HIREDATE,SAL,COMM,DEPTNO) values (999,'HECK','CLERK',7902,to_date('17-DEC-80 12.00.00','DD-MON-RR HH.MI.SS'),800,null,20);
+
+exec DBMS_MVIEW.refresh('SCOTTS_EMP_MV');
+
+drop MATERIALIZED VIEW emp_mv;
+delete from scott.emp where empno=999;
+
+---
+
+-- https://asktom.oracle.com/pls/asktom/f?p=100:11:0::::P11_QUESTION_ID:9538465900346087401
 create materialized view mv as 
   select * from t;
   
