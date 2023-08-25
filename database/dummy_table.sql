71bf9029eef362c1a84119db17fee983cb4b95cf
Bob Heckel <rsh@rshdev.com>
2023-08-25 15:44:29 -0400

    mod gitall x86_64-unknown-cygwin-l10g

--- database/dummy_table.sql
+++ database/dummy_table.sql
@@ -26,7 +26,7 @@ insert into SALESGROUP_SUBSIDIARY ( SALESGROUP_SUBSIDIARY_ID, SALESGROUP, SUBSID
   select uid_salesgroup_subsidiary.nextval, s.SALESGROUP, s.SUBSIDIARY_NAME, s.ADMINISTERING_COUNTRY_CD, s.STATUS, s.REVSTATE, o.SUBSIDIARYINC
     from SALESGROUP_SUBSIDIARY s, ( with v as ( select 'INCAU' SUBSIDIARYINC, 'SASA' SUBSIDIARY, 'SAS In' SUBSIDIARY_NAME from dual 
                                                 union all
-                                                select 'INCAU' SUBSIDIARYINC, 'SASA' SUBSIDIARY, 'SAS In' SUBSIDIARY_NAME from dual ) 
+                                                select 'INCA2' SUBSIDIARYINC, 'SAS2' SUBSIDIARY, 'SAS In' SUBSIDIARY_NAME from dual ) 
                                     select * from v ) o
    where s.subsidiary = o.subsidiary
 ;
