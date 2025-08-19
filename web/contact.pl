f72f5beb53430d5a035f8103cd3831f92a504527
Bob Heckel <b.heckel@gmail.com>
2025-08-19 14:10:34 -0400

    mod gitall x86_64-pc-linux-gnu-mett

--- web/contact.pl
+++ web/contact.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
-# Modified: 16-Aug-2025 (Bob Heckel)
+# Modified: 18-Aug-2025 (Bob Heckel)
+# export REQUEST_METHOD=POST && export CONTENT_LENGTH=21 && echo "emssage=CLI%20test%20message" | perl contact.pl
 
 use strict;
 use warnings;
@@ -10,25 +11,30 @@ use MIME::Lite;
 my $q = CGI->new;
 
 my $email   = $q->param('email')   || '';
-my $message = $q->param('message') || '';
+# Typo to discourage bots
+my $emssage = $q->param('emssage') || '';
+
+# Clean up whitespace
+# $emssage =~ s/^\s+|\s+$//g;
 
 my $body = <<"END_MSG";
 Email: $email
 
 Message:
-$message
+$emssage
 END_MSG
 
 my $msg = MIME::Lite->new(
     From    => 'webserver@rshdev.com',
-    To      => 'bheckel@gmail.com,rsh@rshdev.com',
+    To      => 'rsh@rshdev.com',
+    Bcc     => 'bheckel@gmail.com',
     Subject => 'rshdev.com website inquiry',
     Data    => $body
 );
 
-if (defined $body && $body ne '') {
-  $msg->send;
-}
+ if (length($emssage) > 1) {
+   $msg->send;
+ }
 
 # Output confirmation page
 print $q->header('text/html');
@@ -38,7 +44,7 @@ print <<"HTML";
 <head>
 <title>Message Sent</title>
 </head>
-<body bgcolor=#0ea5e9>
+<body bgcolor=#43b0e0>
 <h2>...sent!</h2>
 <p>We will reply to your message in 3 days or less</p>
 </body>
